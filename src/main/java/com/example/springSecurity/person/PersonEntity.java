5f833580ffba590c4826ee5e5c86dc304f518f70
kasra <k.sorouri@gmail.com>
2022-09-13 22:49:03 +0300

    Configute JPA, Add Person Class

---
+++ src/main/java/com/example/springSecurity/person/PersonEntity.java
@@ -0,0 +1,117 @@
+package com.example.springSecurity.person;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "persons",
+        uniqueConstraints ={
+        @UniqueConstraint(name  = "identificationId",
+        columnNames = "ID_Number")
+        } )
+public class PersonEntity {
+    @Id
+    @SequenceGenerator(name = "person_seq",
+            sequenceName = "person_seq",
+            allocationSize = 1
+    )
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "person_seq")
+    @Column(name = "person_Id")
+    public Long personID;
+    @Column(name = "firstName",nullable = false)
+    public String firstName;
+    @Column(name = "lastName",nullable = false)
+    public String lastName;
+    @Column(name = "ID_Number",nullable = true)
+    public String identificationID;
+    @Column(name = "address")
+    public String address;
+    @Column(name = "email")
+    public String email;
+    @Column(name = "phone")
+    public String phoneNumber;
+
+    public PersonEntity(Long personID,
+                        String firstName,
+                        String lastName,
+                        String identificationID,
+                        String address,
+                        String email,
+                        String phoneNumber) {
+        this.personID = personID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.identificationID = identificationID;
+        this.address = address;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public PersonEntity() {
+    }
+
+    public long getPersonID() {
+        return personID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getIdentificationID() {
+        return identificationID;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setIdentificationID(String identificationID) {
+        this.identificationID = identificationID;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonEntity{" +
+                "personID=" + personID +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", identificationID='" + identificationID + '\'' +
+                ", address='" + address + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+}

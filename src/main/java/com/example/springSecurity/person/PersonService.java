e526bbccb41adef118c1bc75313841e68f2a9058
kasra <k.sorouri@gmail.com>
2022-09-22 01:36:29 +0300

    PersonService - organize code

--- src/main/java/com/example/springSecurity/person/PersonService.java
+++ src/main/java/com/example/springSecurity/person/PersonService.java
@@ -26,11 +26,10 @@ public class PersonService {
         boolean emailCheck ;
         boolean phoneCheck ;
 
+        lastnameCheck = (person.getLastName() !=null && !person.getLastName().isBlank()) ? true : false;
         identificationCheck = (person.getIdentificationID() != null && !person.getIdentificationID().isBlank()) ? !personRepository.findByIdentificationID(person.getIdentificationID()).isPresent() : true;
         emailCheck = (person.getEmail() != null && !person.getEmail().isBlank()) ? !personRepository.findByEmail(person.getEmail()).isPresent() : true;
         phoneCheck = (person.getPhoneNumber() != null && !person.getPhoneNumber().isBlank()) ? !personRepository.findByPhoneNumber(person.getPhoneNumber()).isPresent() : true;
-
-        lastnameCheck = (person.getLastName() !=null && !person.getLastName().isBlank()) ? true : false;
 
         if (!lastnameCheck) {
             throw new IllegalStateException("Lastname should be provided!");
@@ -48,31 +47,7 @@ public class PersonService {
         return inputCheck;
     }
     public PersonEntity addNewPerson(PersonEntity person) {
- /*
-        boolean identificationCheck = false;
-        boolean emailCheck = false;
-        boolean phoneCheck = false;
 
-        identificationCheck = (person.getIdentificationID() != null && !person.getIdentificationID().isBlank()) ? personRepository.findByIdentificationID(person.getIdentificationID()).isPresent() : false;
-        emailCheck = (person.getEmail() != null && !person.getEmail().isBlank()) ? personRepository.findByEmail(person.getEmail()).isPresent() : false;
-        phoneCheck = (person.getPhoneNumber() != null && !person.getPhoneNumber().isBlank()) ? personRepository.findByPhoneNumber(person.getPhoneNumber()).isPresent() : false;
-
-        boolean lastnameCheck = person.getLastName().isBlank();
-
-        if (lastnameCheck) {
-           throw new IllegalStateException("Lastname should be provided!");
-        }
-        if (identificationCheck) {
-            throw new IllegalStateException("A person with this identification number is registered before!");
-        }
-        if (emailCheck) {
-            throw new IllegalStateException("This Email is registered before!");
-        }
-        if (phoneCheck) {
-            throw new IllegalStateException("This phone number is registered before!");
-        }
-
-  */
         if (inputCheck(person)) {
             personRepository.save(person);
             return person;
